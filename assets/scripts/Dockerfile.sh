2d5cdb067d67756f7a81ae7563fa3494016e7f88
wiseupdata <silvio.liborio@wiseupdata.com>
2023-10-24 19:15:00 +0100

    sync

--- assets/scripts/Dockerfile.sh
+++ assets/scripts/Dockerfile.sh
@@ -2,108 +2,107 @@
 # Oficial Python Image based in Ubuntu
 #########################################################
 # Ubuntu 22.04.2 LTS
-FROM wiseupdata/python:3.10
+docker run -it --name python --rm wiseupdata/python:3.10 bash
 
-LABEL maintainer="silvio liborio"
+maintainer="silvio liborio"
 
 
 ###############################################################################
-RUN echo "Initial setup and envs" 
+echo "Initial setup and envs" 
 ##############################################################################
-USER root
+sudo su
 
-ENV DEBIAN_FRONTEND noninteractive
-ENV TERM linux
+DEBIAN_FRONTEND=noninteractive
 
-ENV PYTHON_VERSION="3.10"
-ENV AIRFLOW_VERSION=2.6.1
-ARG AIRFLOW_HOME=/opt/airflow
+PYTHON_VERSION="3.10"
+AIRFLOW_VERSION=2.6.1
+AIRFLOW_HOME=/opt/airflow
 
-RUN echo "Clean up unnecessary files and directories"
+echo "Clean up unnecessary files and directories"
 
-RUN rm -rf /tmp/* && \
+rm -rf /tmp/* && \
     apt autoremove -y && \
     apt clean && \
     rm -rf /var/lib/apt/lists/* && \
     rm -rf /var/cache/apt/*
 
-RUN apt-get update && apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 
-RUN apt-get install netcat -y
-
-##############################################################################
-RUN echo "Creating and Managing the user Airflow"
-##############################################################################
-RUN adduser airflow --disabled-password && usermod -aG sudo airflow
-RUN echo "airflow ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/airflow
-
+apt-get install netcat -y
 
 ##############################################################################
-RUN echo "creating the directories"
+echo "Creating and Managing the su airflow"
 ##############################################################################
-RUN mkdir $AIRFLOW_HOME \
+addsu airflow --disabled-password && usermod -aG sudo airflow
+echo "airflow ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/airflow
+
+
+##############################################################################
+echo "creating the directories"
+##############################################################################
+mkdir $AIRFLOW_HOME \
     && mkdir $AIRFLOW_HOME/logs \
     && mkdir $AIRFLOW_HOME/dags \
     && mkdir $AIRFLOW_HOME/plugins
 
 
 ##############################################################################
-RUN echo "preparing the artifacts and configures"
+echo "preparing the artifacts and configures"
 ##############################################################################
 COPY scripts/entrypoint.sh /entrypoint.sh
 COPY config/constraints-3.10.txt /constraints-3.10.txt
 COPY config/requirements.txt $AIRFLOW_HOME/dags/requirements.txt
-RUN sudo chown -R airflow:airflow ${AIRFLOW_HOME}
+sudo chown -R airflow:airflow ${AIRFLOW_HOME}
 
 ##############################################################################
-RUN echo "installing the python dependencies"
+echo "installing the python dependencies"
 ##############################################################################
-USER airflow
+su airflow
 
-RUN pip install -r $AIRFLOW_HOME/dags/requirements.txt
+pip install -r $AIRFLOW_HOME/dags/requirements.txt
 
 
 ##############################################################################
-RUN echo "installing Apache Airflow"
+echo "installing Apache Airflow"
 ##############################################################################
-ARG CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
-RUN pip install -i https://pypi.org/simple/ pyarrow
-RUN pip install "apache-airflow[aws,crypto,celery,jira,amazon,postgres,http,jdbc,password,redis,ssh,google,databricks]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+pip install -i https://pypi.org/simple/ pyarrow
+pip install "apache-airflow[aws,crypto,celery,jira,amazon,postgres,http,jdbc,password,redis,ssh,google,databricks]==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
 
-RUN export PATH="$PATH:/home/airflow/.local/bin"
+export PATH="$PATH:/home/airflow/.local/bin"
 
 
 # ##############################################################################
-# RUN echo "Set PATH environment variable for all users"
+# echo "Set PATH environment variable for all users"
 # ##############################################################################
-# RUN echo 'export PATH="$HOME/.local/bin:$PATH"' >> /etc/profile
+# echo 'export PATH="$HOME/.local/bin:$PATH"' >> /etc/profile
 
 
 ##############################################################################
-RUN echo "ensuring the correct owner of the environment"
+echo "ensuring the correct owner of the environment"
 ##############################################################################
-USER root
-RUN chown -R airflow:airflow ${AIRFLOW_HOME}
+sudo su
+chown -R airflow:airflow ${AIRFLOW_HOME}
 
 
 ##############################################################################
-RUN echo "exposing the port for run the services"
+echo "exposing the port for the services"
 ##############################################################################
 EXPOSE 8080 5555 8793
 
 
 ##############################################################################
-RUN echo "Final steps"
+echo "Final steps"
 ##############################################################################
-RUN  chmod a+x /entrypoint.sh
+ chmod a+x /entrypoint.sh
 
-RUN rm -rf /tmp/* && \
+rm -rf /tmp/* && \
     apt autoremove -y && \
     apt clean && \
     rm -rf /var/lib/apt/lists/* && \
     rm -rf /var/cache/apt/*
 
-USER airflow
-WORKDIR ${AIRFLOW_HOME}
-ENTRYPOINT ["/entrypoint.sh"]
-CMD ["webserver"]
+su airflow
+cd ${AIRFLOW_HOME}
+sh /entrypoint.sh
+webserver
